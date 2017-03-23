322d2f9652c273a169927467e61cadcabcdb8868
Daria Efimova <d.efimova@vectordigital.ru>
2017-03-23 14:52:10 +0100

    added progress bar to the recipe list screen

--- app/src/main/java/com/cookplan/recipe_grid/RecipeGridFragment.java
+++ app/src/main/java/com/cookplan/recipe_grid/RecipeGridFragment.java
@@ -1,6 +1,7 @@
 package com.cookplan.recipe_grid;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -9,10 +10,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.cookplan.BaseActivity;
 import com.cookplan.R;
 import com.cookplan.models.Recipe;
+import com.cookplan.recipe_view.RecipeViewActivity;
 import com.cookplan.utils.GridSpacingItemDecoration;
 import com.cookplan.utils.Utils;
 
@@ -23,6 +27,7 @@ public class RecipeGridFragment extends Fragment implements RecipeGridView {
 
     private RecipeGridRecyclerViewAdapter adapter;
     private RecipeGridPresenter presenter;
+    private ViewGroup mainView;
 
     public RecipeGridFragment() {
     }
@@ -47,25 +52,31 @@ public class RecipeGridFragment extends Fragment implements RecipeGridView {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_recipe_list, container, false);
+        mainView = (ViewGroup) inflater.inflate(R.layout.fragment_recipe_list, container, false);
 
         // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
+        RecyclerView recyclerView = (RecyclerView) mainView.findViewById(R.id.recipe_list_recycler);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, Utils.dpToPx(getActivity(), 16), true));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new RecipeGridRecyclerViewAdapter(new ArrayList<>(), recipe -> {
+            Activity activity = getActivity();
+            if (activity instanceof BaseActivity) {
+                Intent intent = new Intent(activity, RecipeViewActivity.class);
+                intent.putExtra(RecipeViewActivity.RECIPE_OBJECT_KEY, recipe);
+                ((BaseActivity) activity).startActivityWithLeftAnimation(intent);
+            }
+        });
+        recyclerView.setAdapter(adapter);
 
-            recyclerView.setLayoutManager(new GridLayoutManager(context, 2));
-            recyclerView.addItemDecoration(new GridSpacingItemDecoration(2, Utils.dpToPx(getActivity(), 16), true));
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-            adapter = new RecipeGridRecyclerViewAdapter(new ArrayList<>());
-            recyclerView.setAdapter(adapter);
-        }
 
-        return view;
+        return mainView;
     }
 
     @Override
     public void setRecipeList(List<Recipe> recipeList) {
+        ProgressBar progressBar = (ProgressBar) mainView.findViewById(R.id.progress_bar);
+        progressBar.setVisibility(View.GONE);
         adapter.updateItems(recipeList);
     }
 
@@ -73,6 +84,4 @@ public class RecipeGridFragment extends Fragment implements RecipeGridView {
     public void setErrorToast(String error) {
         Toast.makeText(getActivity(), error, Toast.LENGTH_LONG).show();
     }
-
-
 }
