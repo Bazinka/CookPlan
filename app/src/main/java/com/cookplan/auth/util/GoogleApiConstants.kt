2fc3df497a94aba1167e287a08d5d924f9a71b8b
Daria Efimova <d.efimova@vectordigital.ru>
2017-11-15 15:19:08 +0100

    fix auth constants file

---
+++ app/src/main/java/com/cookplan/auth/util/GoogleApiConstants.kt
@@ -0,0 +1,8 @@
+package com.cookplan.auth.util
+
+import android.support.annotation.RestrictTo
+
+@RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+object GoogleApiConstants {
+    val AUTO_MANAGE_ID0 = 0
+}
